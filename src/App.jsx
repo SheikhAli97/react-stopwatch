6d44aae91e42ddbeb102486e8fc8b74294df9990
Abdurrahman Mohamoud <abdurrahman.mohamoud@weareadaptive.com>
2022-10-07 11:32:27 +0100

    Get timer ticking

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Stopwatch from './components/Stopwatch'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (<div>
   <Stopwatch />
--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,11 +1,108 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Stopwwatch.css";
 
 function Stopwatch() {
-  return (
+  const [totalTime, setTotalTime] = useState(0);
+  let [watchIsOn, setWatchIsOn] = useState(false);
 
-    
-    <div>Stopwatch</div>
-  )
+  const formatTime = (time) => {
+    let formattedMilliseconds = 0;
+    let formattedSeconds = 0;
+    let formattedMinutes = 0;
+    let formattedTime = [];
+    let finalTime = ``;
+    formattedMilliseconds = time % 100;
+    formattedSeconds = Math.floor(time / 100);
+    formattedMinutes = Math.floor(formattedSeconds / 60);
+
+    formattedSeconds = formattedSeconds % 60;
+    formattedMinutes = formattedMinutes % 60;
+
+    formattedTime = [
+      formattedMinutes,
+      formattedSeconds,
+      formattedMilliseconds,
+    ].map(padNumber);
+    finalTime = `${formattedTime[0]}:${formattedTime[1]}.${formattedTime[2]}`;
+    return finalTime;
+  };
+
+  const padNumber = (time) => {
+    return time.toString().padStart(2, "0");
+  };
+
+  function toggleWatchStatus() 
+  {
+    setWatchIsOn(!watchIsOn);
+  }
+
+const reset = () =>{
+  setTotalTime(0); 
+  setWatchIsOn(false);
 }
 
-export default Stopwatch
+function startTime(){
+  toggleWatchStatus();
+}
+
+
+
+
+
+  useEffect(() => {
+    let intervalID;
+    let initialTime = Date.now
+
+    if (watchIsOn) {
+      intervalID = setInterval(() => {
+        setTotalTime((totalTime) => totalTime + 1);
+        console.log(totalTime)
+      }, 10);
+    } 
+
+    if (!watchIsOn) {
+      clearInterval(intervalID)
+    }
+
+
+    return () => clearInterval(intervalID);
+  }, [watchIsOn, totalTime]); //run use effect if watch status or time changes
+
+
+
+
+
+
+
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="main">
+          <div id="timer" className="main-timer">
+            {formatTime(totalTime)}
+          </div>
+          <div className="button-container">
+            <div className="button-wrapper">
+              <button id="reset" className="initial-lap">
+                Lap
+              </button>
+            </div>
+            <div className="button-wrapper">
+              <button id="start" className="start-button" onClick={startTime}>
+                Start
+              </button>
+            </div>
+          </div>
+
+          <div className="laps-container">
+            <table id="laps-table" className="laps-table">
+             
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
