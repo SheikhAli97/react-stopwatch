3aed48770f245e41572ef9d06309f70115fad138
Abdurrahman Mohamoud <abdurrahman.mohamoud@weareadaptive.com>
2022-10-10 13:02:47 +0100

    fixed formatting function bug

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -5,79 +5,49 @@ function Stopwatch() {
   const [totalTime, setTotalTime] = useState(0);
   const [watchIsOn, setWatchIsOn] = useState(false);
 
-  const formatTime = (time) => {
-    let formattedMilliseconds = 0;
-    let formattedSeconds = 0;
-    let formattedMinutes = 0;
-    let formattedTime = [];
-    let finalTime = ``;
-    formattedMilliseconds = time % 1000;
-    formattedSeconds = Math.floor(time / 1000);
-    formattedMinutes = Math.floor(formattedSeconds / 60);
+  const padNumber = (value) => Math.floor(value).toString().padStart(2, "0");
 
-    formattedSeconds = formattedSeconds % 60;
-    formattedMinutes = formattedMinutes % 60;
-
-    formattedTime = [
-      formattedMinutes,
-      formattedSeconds,
-      formattedMilliseconds,
-    ].map(padNumber);
-    finalTime = `${formattedTime[0]}:${formattedTime[1]}.${formattedTime[2]}`;
-    return finalTime;
+  const formatTime = (elapsedTime) => {
+    const centiseconds = (elapsedTime % 1000) / 10;
+    const seconds = (elapsedTime / 1000) % 60;
+    const minutes = (elapsedTime / (1000 * 60)) % 60;
+    return `${padNumber(minutes)}:${padNumber(seconds)}.${padNumber(
+      centiseconds
+    )}`;
   };
 
-  const padNumber = (time) => {
-    return time.toString().padStart(2, "0").substring(0,2);
-  };
-
-  const toggleWatchStatus =() => 
-  {
+  const toggleWatchStatus = () => {
     setWatchIsOn(!watchIsOn);
-  }
+  };
 
-const handleReset = () =>{
-   setTotalTime(0);
-  setWatchIsOn(false);
-}
+  const handleReset = () => {
+    setTotalTime(0);
+    setWatchIsOn(false);
+  };
 
-const startTime =()=>{
-  toggleWatchStatus();
-}
+  const startTime = () => {
+    toggleWatchStatus();
+  };
 
-const calculateLap =  () => {
-console.log('calculate lap')
-}
+  const calculateLap = () => {
+    console.log("calculate lap");
+  };
 
+  useEffect(() => {
+    let intervalID;
 
-
-
-useEffect(() => {
-  let intervalID;
-
-  
-  if (watchIsOn) {
-    const initialTime = Date.now() - totalTime;
-    intervalID = setInterval(() => {
-
-        const currentTime = Date.now()
-        const elapsedTime = currentTime - initialTime; 
+    if (watchIsOn) {
+      const initialTime = Date.now() - totalTime;
+      intervalID = setInterval(() => {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - initialTime;
         setTotalTime(elapsedTime);
-       console.log(elapsedTime)
-      
+        console.log(elapsedTime);
       }, 10);
-    } 
-
-
+    }
 
     return () => clearInterval(intervalID);
   }, [watchIsOn]); //run use effect if watch status or time changes
-
-
-
-
-
-
 
   return (
     <div>
@@ -88,22 +58,31 @@ useEffect(() => {
           </div>
           <div className="button-container">
             <div className="button-wrapper">
-              <button id="reset" className='initial-lap' onClick={watchIsOn ? calculateLap : handleReset}>
-                { watchIsOn ? 'Lap' : 'Reset' && totalTime === 0 ? 'Lap' : 'Reset'}
-                
+              <button
+                id="reset"
+                className="initial-lap"
+                onClick={watchIsOn ? calculateLap : handleReset}
+              >
+                {watchIsOn
+                  ? "Lap"
+                  : "Reset" && totalTime === 0
+                  ? "Lap"
+                  : "Reset"}
               </button>
             </div>
             <div className="button-wrapper">
-              <button id="start" className={watchIsOn ? "stop-button" : "start-button"} onClick={startTime}>
-                {watchIsOn ? 'Stop' : 'Start'}
+              <button
+                id="start"
+                className={watchIsOn ? "stop-button" : "start-button"}
+                onClick={startTime}
+              >
+                {watchIsOn ? "Stop" : "Start"}
               </button>
             </div>
           </div>
 
           <div className="laps-container">
-            <table id="laps-table" className="laps-table">
-             
-            </table>
+            <table id="laps-table" className="laps-table"></table>
           </div>
         </div>
       </div>
