82a53fd5721f34d152a050136f941ce2aaa347ff
Abdurrahman Mohamoud <abdurrahman.mohamoud@weareadaptive.com>
2022-10-13 09:06:37 +0100

    calculate and render laps

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -5,7 +5,16 @@ function Stopwatch() {
   const [totalTime, setTotalTime] = useState(0);
   const [watchIsOn, setWatchIsOn] = useState(false);
 
-  const padNumber = (value) => Math.floor(value).toString().padStart(2, "0");
+  const [lapState, setLapState] = useState({
+    laps: [],
+    runningTime: 0,
+    totalLapTime: 0,
+    slowestLap: [],
+    fastestLap: [],
+  });
+  
+
+  const padNumber = (time) => Math.floor(time).toString().padStart(2, "0");
 
   const formatTime = (elapsedTime) => {
     const centiseconds = (elapsedTime % 1000) / 10;
@@ -22,16 +31,20 @@ function Stopwatch() {
 
   const handleReset = () => {
     setTotalTime(0);
-    setWatchIsOn(false);
+    setLapState({
+      laps: [],
+      runningTime: 0,
+      totalLapTime: 0,
+      slowestLap: [],
+      fastestLap: [],
+    });
   };
 
   const startTime = () => {
     toggleWatchStatus();
   };
 
-  const calculateLap = () => {
-    console.log("calculate lap");
-  };
+  //check if lap button has been clicked.
 
   useEffect(() => {
     let intervalID;
@@ -42,12 +55,29 @@ function Stopwatch() {
         const currentTime = Date.now();
         const elapsedTime = currentTime - initialTime;
         setTotalTime(elapsedTime);
-        console.log(elapsedTime);
+        setLapState((previousLapState) => ({
+          ...previousLapState,
+          runningTime: elapsedTime - previousLapState.totalLapTime,
+        }));
       }, 10);
     }
 
     return () => clearInterval(intervalID);
   }, [watchIsOn]); //run use effect if watch status or time changes
+
+  const addLap = () => {
+    //add lap to the
+    if (totalTime > 0) {
+      setLapState((previousLapData) => ({
+        ...previousLapData,
+        laps: [...previousLapData.laps, lapState.runningTime],
+        totalLapTime:
+          previousLapData.totalLapTime + previousLapData.runningTime,
+      }));
+    }
+
+    console.log(lapState);
+  };
 
   return (
     <div>
@@ -60,8 +90,8 @@ function Stopwatch() {
             <div className="button-wrapper">
               <button
                 id="reset"
-                className="initial-lap"
-                onClick={watchIsOn ? calculateLap : handleReset}
+                className="initial-lap button"
+                onClick={watchIsOn ? addLap : handleReset}
               >
                 {watchIsOn
                   ? "Lap"
@@ -73,7 +103,9 @@ function Stopwatch() {
             <div className="button-wrapper">
               <button
                 id="start"
-                className={watchIsOn ? "stop-button" : "start-button"}
+                className={
+                  watchIsOn ? "stop-button button" : "start-button button"
+                }
                 onClick={startTime}
               >
                 {watchIsOn ? "Stop" : "Start"}
@@ -82,7 +114,25 @@ function Stopwatch() {
           </div>
 
           <div className="laps-container">
-            <table id="laps-table" className="laps-table"></table>
+            <table id="laps-table" className="laps-table">
+              <tbody>
+                {(watchIsOn || totalTime > 0) && (
+                  <tr className="table-row">
+                    <td> Lap {lapState.laps.length + 1}</td>
+                    <td> {formatTime(lapState.runningTime)}</td>
+                  </tr>
+                )}
+
+                {lapState.laps.map((lap, index) => {
+                  return (
+                    <tr key={index} className="table-row">
+                      <td>Lap {index + 1}</td>
+                      <td>{formatTime(lap)}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
